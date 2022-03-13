9f383ddb1bb14096ae4fd104e9dd96e67a68d8f8
IoJupiterI <36616714+IoJupiterI@users.noreply.github.com>
2022-03-13 19:56:22 +0800

    update data

--- src/common.ts
+++ src/common.ts
@@ -2,4 +2,4 @@
 import { parseUnits } from "ethers/lib/utils";
 
 export const metamTotalSupply = parseUnits("595170.937195", "gwei").sub(parseUnits("134927.522259159", "gwei"));
-export const totalBribe = parseUnits("83000", "6");
+export const totalBribe = parseUnits("81300", "6");
--- src/gen-md.ts
+++ src/gen-md.ts
@@ -1,13 +1,15 @@
 import { readFileSync, writeFileSync } from "fs";
 import { formatUnits } from "ethers/lib/utils";
-import { totalBribe } from "./common";
+import { metamTotalSupply, totalBribe } from "./common";
 
 let md = `
 # Bribe Data
 
 Total Eligible Address: %0
 
-Total USDC for bribe: %1
+Total \`USDC\` for bribe: %1
+
+Total \`MetaM\` Eligible: %2
 
 |Address|USDC|
 |---|---|
@@ -23,7 +25,9 @@ function main() {
     }
   } = JSON.parse(readFileSync(`output/list.json`).toString());
   const keys = Object.keys(data);
-  md = md.replace("%0", String(keys.length)).replace("%1", formatUnits(totalBribe, "6"));
+  md = md.replace("%0", String(keys.length))
+    .replace("%1", formatUnits(totalBribe, "6"))
+    .replace("%2", formatUnits(metamTotalSupply, "gwei"));
   for (let addr of keys) {
     md += `|${addr}|${formatUnits(data[addr].usdc, "6")}|\n`;
   }
